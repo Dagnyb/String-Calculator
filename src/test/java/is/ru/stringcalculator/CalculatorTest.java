eb32180a7de2a23be04ec727cfc33abdb9bc988c
Dagny Birgisdottir <Dagny Birgisdottir>
2016-10-14 00:36:42 +0000

    Update test new Line

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -31,7 +31,7 @@ public class CalculatorTest {
 
     @Test
     public void testNewLine(){
-    	assertEquals(6, Calculator.add("1,2,3"));
+    	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
 }
